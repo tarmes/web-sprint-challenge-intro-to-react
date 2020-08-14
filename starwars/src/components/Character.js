43ba7feb9a430cd85c95a0d26e04af28641625af
tarmes <traarmes@gmail.com>
2020-08-14 13:49:30 -0400

    Almost MVP

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from 'react';
+
+export default function Character(props){
+    const {characterObj} = props
+
+    // const StyledCharacter = styled.div`
+    
+    // h1{
+    //     color: ${props => {
+    //     const { theme } = props;
+    //     
+    // }};
+    // }
+    
+    // `
+
+    return(
+        <Character>
+        <h1>Name: {characterObj.name} Height: {characterObj.height} Mass: {characterObj.mass}</h1>
+        </Character>
+    )
+}
